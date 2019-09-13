ee2fea59e46486896a9e2cd15dc9d341e1fcecf3
Tatyana M Gulina <tt@hlobys.ru>
2019-09-13 15:19:04 +0300

    Dublicate svgs are impossible now.

--- index.js
+++ index.js
@@ -9,19 +9,31 @@ const Sprite = require('svg-baker/lib/sprite');
 const extension = '.svg'
 
 let svgCompiler = new SVGCompiler();
+const svgReplaceObjects = [];
 
 module.exports = function svgSpriteLoader (options = {}) {
   return {
     name: 'rollup-svg-sprite-loader',
     async load (id) {
       if (path.extname(id) !== extension)
-        return null
+        return null;
 
       const data = readFileSync( id, 'UTF-8' );
-      const useId = nanoid();
-      const symbol = await svgCompiler.addSymbol({ id: useId, content: data, path: id })
+
+      let svgReplace = svgReplaceObjects.find(svg => svg.content === data);
+      if (!svgReplace) {
+        const useId = nanoid();
+        const symbol = await svgCompiler.addSymbol({ id: useId, content: data, path: id });
+        svgReplace = {
+            id: JSON.stringify(useId),
+            content: data,
+            viewBox: symbol.viewBox,
+        };
+          svgReplaceObjects.push(svgReplace)
+      }
+
       // eslint-disable-next-line max-len
-      const code = `const img = {id: ${JSON.stringify(useId)}, content: ${JSON.stringify(data)}, viewbox: ${JSON.stringify(symbol.viewBox || '')} }; export default img;`
+      return`var img = {id: ${svgReplace.id}, content: ${JSON.stringify(svgReplace.content)}, viewbox: ${JSON.stringify(svgReplace.viewBox)} }; export default img;`;
 
       const ast = {
         type: 'Program',
@@ -38,11 +50,11 @@ module.exports = function svgSpriteLoader (options = {}) {
       const sprite = await Sprite.create({
         symbols: svgCompiler.symbols,
         factory: spriteFactory
-      })
-      const content = sprite.render()
+      });
+      const content = sprite.render();
       // eslint-disable-next-line max-len
-      const svgNodeInsert = 'if (window) { window.document.addEventListener(\'DOMContentLoaded\', function(){ window.document.body.prepend('+ JSON.stringify(content) + ') }); }'
-      const replacedCode = code + svgNodeInsert
+      const svgNodeInsert = 'if (window) { window.document.addEventListener(\'DOMContentLoaded\', function(){ const div = document.createElement(\'div\'); div.setAttribute(\'style\', \'display: none; height:0; width: 0; overflow: hidden;\');  div.innerHTML = '+ JSON.stringify(content) + ';  window.document.body.appendChild(div) }); }'
+      const replacedCode = code + svgNodeInsert;
 
       return { code: replacedCode }
     }
